9fe3a7dc48b275361e9884b2fd3a2890bc3c0ebf
Sathya Narayan <snp@Sathyas-MacBook-Air.local>
2023-09-11 13:23:09 +0530

    Used states to handle forms

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,6 +11,15 @@ export default function Form(props) {
             memeImage:"https://i.imgflip.com/1bij.jpg"
         }
     )
+    function handleChange(event){
+        const {name,value} = event.target
+        setMeme(prevData => {
+            return {
+                ...prevData,
+                [name]:value
+            }
+        })
+    }
     function generateMeme(){
         console.log(memesData.data.memes.length)
         const randNum = Math.floor(Math.random() * memesData.data.memes.length)
@@ -21,20 +30,20 @@ export default function Form(props) {
                 memeImage:randMemeImg
             }
         })
-        console.log(memeImage)
+        console.log(meme)
         
 
     }
     return(
         <main>
             <div className="form" >
-                <input className="ip1" type="text" name="" id="" placeholder="Top Text"/>
-                <input className="ip2" type="text" name="" id="" placeholder="Bottom Text" />
+                <input value={meme.topText}  onChange={handleChange} className="ip1" type="text" name="topText" id="" placeholder="Top Text"/>
+                <input value={meme.bottomText} onChange={handleChange} className="ip2" type="text" name="bottomText" id="" placeholder="Bottom Text" />
                 <button class="btn" onClick={generateMeme}>
                 Get a new meme image 🖼
                 </button>
             </div>
-            <Meme image={meme.memeImage} />
+            <Meme topText={meme.topText} bottomText={meme.bottomText} image={meme.memeImage} />
 
 
         </main>
